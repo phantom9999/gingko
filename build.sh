f05f6ae2de339cf305d82d3db0cefeb8855e7ff2
phantom9999 <1057369361@qq.com>
2017-07-11 23:58:43 +0800

    mv file

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+WORKSPACE=`pwd`
+PROTOC=protoc
+THRIFT=thrift
+
+
+
+file_list=`find $WORKSPACE -name *.proto`
+for file_item in $file_list
+do
+    echo $file_item
+    file_dirname=`dirname $file_item`
+    $PROTOC --cpp_out=$file_dirname --proto_path=$file_dirname $file_item
+done
+
+
+file_list=`find $WORKSPACE -name *.thrift`
+for file_item in $file_list
+do
+    echo $file_item
+    file_dirname=`dirname $file_item`
+    $THRIFT -r --gen cpp -out $file_dirname -I $file_dirname $file_item
+done
+
+
+
+rm `find  $WORKSPACE -name *.skeleton.cpp`
